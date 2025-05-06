a85e74baf8084ecbf96b7d62720aea46c5166020
LiaCoder13 <voronova.svetlana161004@gmail.com>
2025-05-06 17:18:39 +0500

    Добавлено наследование для теста перехода на страницу логина

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 import time
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -21,6 +22,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.should_be_product_added_to_basket()
 
 @pytest.mark.xfail
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
@@ -28,6 +30,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.add_to_basket()
     product_page.should_not_be_success_message()
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
@@ -35,9 +38,24 @@ def test_guest_cant_see_success_message(browser):
     product_page.should_not_be_success_message()
 
 @pytest.mark.xfail
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
     product_page.should_disappear_success_message()
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
